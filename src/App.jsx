2ab526409d18506c260bf3d7b96598f6ed21ce71
BorisTorres <bogui231297@gmail.com>
2024-05-24 23:35:56 -0500

    practica 1 creado y subido a git

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='conteiner'>
+        <div className='left'>
+          <h1>Welcome to the</h1>
+          <h1>Frontend Quiz!</h1>
+          <p>pick a subject to get started.</p>
+        </div>
+        <div className='rigth'>
+          <div className='html'>
+            <div className='conteiner-html'>
+              <img src="../public/images/codigo.png" alt="" />
+            </div>
+            <p>HTML</p>
+          </div>
+          <div className='css'>
+            <div className='conteiner-css'>
+              <img src="../public/images/brocha.png" alt="" />
+            </div>
+            <p>CSS</p>
+          </div>
+          <div className='javascript'>
+            <div className='conteiner-java'>
+              <img src="../public/images/javascript.png" alt="" />
+            </div>
+            <p>Javascript</p>
+          </div>
+          <div className='Accessibility'>
+            <div className='conteiner-access'>
+              <img src="../public/images/accesibilidad.png" alt="" />
+            </div>
+            <p>Accessibility</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
